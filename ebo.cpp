90c5699c94188a668d41a00e425c0044eb14fdeb
16SAGG <sagg160800@gmail.com>
2026-06-30 11:48:19 -0400

    Ebo cleaned

--- ebo.cpp
+++ ebo.cpp
@@ -1,3 +1,5 @@
+#include<glad/glad.h>
+
 #include "ebo.h"
 
 EBO::EBO(const GLuint* indices, const GLsizeiptr size)
@@ -5,13 +7,6 @@ EBO::EBO(const GLuint* indices, const GLsizeiptr size)
 	glGenBuffers(1, &ID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ID);
 
-	/**
-	 * Transfiere los datos de índices de la CPU a la VRAM de la GPU.
-	 * Este comando reserva memoria en la tarjeta gráfica y carga el array de índices.
-	 * Usamos GL_STATIC_DRAW como "hint" al driver para indicar que esta geometría
-	 * es permanente (no cambia en tiempo de ejecución), optimizando su ubicación
-	 * en la memoria de video más rápida.
-	 */
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, indices, GL_STATIC_DRAW);
 }
 
